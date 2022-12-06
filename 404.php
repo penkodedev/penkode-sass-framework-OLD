e664cd9e57dcdcc5671a3cbcdff6d2e9da20b946
Penkode <email@penkode.com>
2022-12-06 02:34:25 +0100

    change

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
     <main class="grid-main" id="main-container">
         <article class="grid-article">
             <h1>ERROR 404</h1>
-        <p><?php _e( 'Al parecer este contenido no existe en nuestra Web. Intente el menú en la barra superior.', 'foo' ); ?></p>
+        <p><?php _e( 'Al parecer este contenido no existe en nuestra Web. Intente el menú en la barra superior.', 'penkode' ); ?></p>
             <p><a href="<?php echo home_url(); ?>"><?php _e( 'Volver al inicio?', 'foo' ); ?></a></p>
         </article>
     </main>
