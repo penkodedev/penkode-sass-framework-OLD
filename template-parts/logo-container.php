5cb1fcbd35f299e69095201be3a4520d5aa6b10b
Penkode <email@penkode.com>
2022-12-06 10:26:47 +0100

    asdf

--- template-parts/logo-container.php
+++ template-parts/logo-container.php
@@ -1,3 +1,2 @@
 <figure id="logo-container"><a href="<?php bloginfo('siteurl'); ?>" title="<?php bloginfo('name'); ?> |<?php bloginfo('description'); ?>."><img src="<?php bloginfo('template_url'); ?>/images/framework-logo.png" /></a>
 </figure>
-
