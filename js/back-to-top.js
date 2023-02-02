a166481d8b238d546669b6b9560a900268d7e1cb
Penkode <email@penkode.com>
2023-02-02 11:41:39 +0100

    spaces

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -1,18 +1,19 @@
 /************* BACK TO TOP ********************/
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     var offset = 500;
     var duration = 600;
-    jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.back-to-top').fadeIn(duration);
-        } else {
-            jQuery('.back-to-top').fadeOut(duration);
-        }
-    });
     
-    jQuery('.back-to-top').click(function(event) {
-        event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-        return false;
-    })
+  jQuery(window).scroll(function () {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery(".back-to-top").fadeIn(duration);
+    } else {
+      jQuery(".back-to-top").fadeOut(duration);
+    }
+  });
+
+  jQuery(".back-to-top").click(function (event) {
+    event.preventDefault();
+    jQuery("html, body").animate({ scrollTop: 0 }, duration);
+    return false;
+  });
 });
