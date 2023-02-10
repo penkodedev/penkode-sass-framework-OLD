4a56062b0ead9f310053f56109626f8798b86ed2
Penkode <email@penkode.com>
2023-02-10 16:47:40 +0100

    Wp_Query on Front Page

--- front-page.php
+++ front-page.php
@@ -1,56 +1,71 @@
-<?php get_template_part('/template-parts/doc-type');?>
+<?php get_template_part('/template-parts/doc-type'); ?>
 <?php
 /*
 Template Name: Index Full Width
 */
 ?>
 <div id="home-full-col"><!-- OPEN GRID / close on footer -->
-	<?php get_header();?>
+	<?php get_header(); ?>
 
 	<main class="grid-main animate fadeIn" id="main-container">
 		<section class="section-01">
 			<article id="article">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-				<?php endwhile; else: ?>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<?php the_content(); ?>
+					<?php endwhile;
+				else : ?>
 				<?php endif; ?>
 			</article>
 		</section>
 
 		<section class="section-02">
-		<article id="article">
-			<div class="post-grid">
-			<?php query_posts(array(
-				'post_type' => 'news',
-				'has_archive' => true,
-				'orderby'=> 'ASC',
-				'paged'=>$paged, )); ?>
+			<article id="article">
+				<div class="post-grid">
 
-					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<div class="post-full col-3">
-						<div class="grid-item">
-							<figure><a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>">
-									<?php the_post_thumbnail('large'); ?></a>
-							</figure>
-							<div class="grid-item-content">
-								<h5>
-									<?php the_title(); ?>
-								</h5>
-								<p class="grid-item-excerpt">
-									<?php echo excerpt('22'); ?>
-								</p>
-								<a class="button" href="<?php the_permalink(); ?>"><?php _e('leer más', 'foo' ); ?></a>
+					<?php
+					$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+					$custom_query = new WP_Query(array(
+						'post_type' => 'news',
+						'has_archive' => true,
+						'orderby' => 'ASC',
+						'paged' => $paged,
+					));
+
+					if ($custom_query->have_posts()) :
+						while ($custom_query->have_posts()) :
+							$custom_query->the_post();
+					?>
+							<div class="post-full col-3">
+								<div class="grid-item">
+									<figure><a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>">
+											<?php the_post_thumbnail('large'); ?></a>
+									</figure>
+									<div class="grid-item-content">
+										<h5>
+											<?php the_title(); ?>
+										</h5>
+										<p class="grid-item-excerpt">
+											<?php echo excerpt('22'); ?>
+										</p>
+										<a class="button" href="<?php the_permalink(); ?>"><?php _e('leer más', 'foo'); ?></a>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-					<?php endwhile; else: ?>
-					<?php endif;
-			wp_reset_query();?>
+					<?php
+						endwhile;
+					else :
+					// No posts found
+					endif;
+
+					// Reset post data
+					wp_reset_postdata();
+					?>
 				</div>
 			</article>
 		</section>
 
-		<?php get_template_part ('/template-parts/carousel'); ?>
+		<?php get_template_part('/template-parts/carousel'); ?>
 
 		<section class="section-03">
 			<article id="article">
